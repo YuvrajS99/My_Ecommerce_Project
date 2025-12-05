ee60a968c8627ebff5be0240ba75c54586726c0c
YuvrajS99 <rajsanap779@gmail.com>
2025-12-05 15:08:47 +0530

    Added New Collections Component box

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Hero from '../Components/Hero/Hero.jsx'
 import Popular from '../Components/Popular/Popular'
 import { Offers } from '../Components/Offers/Offers.jsx'
+import { NewCollections } from '../Components/NewCollections/NewCollections.jsx'
 const Shop = () => {
   return (
     <div>
       <Hero />
       <Popular />
       <Offers />
+      <NewCollections />
 
     </div >
   )
